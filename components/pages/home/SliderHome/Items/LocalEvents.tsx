9d7d478ac8688e5a5e7675c4cfca5f53fe16a76b
kevelscy <kevelscy@gmail.com>
2022-01-12 10:33:51 -0400

    update: agregando blur de los eventos del banner

--- components/pages/home/SliderHome/Items/LocalEvents.tsx
+++ components/pages/home/SliderHome/Items/LocalEvents.tsx
@@ -7,20 +7,22 @@ export const LocalEvents = () => (
     <div className='hidden sm:block'>
       <Image
         src={eventSrcDesktop}
-        alt='La Casa de mi Padre'
+        alt='La Casa de mi Padre - Servicio de Dedicación'
         layout='fill'
         objectFit="cover"
         quality={100}
+        placeholder='blur'
       />
     </div>
 
     <div className='sm:hidden'>
       <Image
         src={eventSrcMobile}
-        alt='La Casa de mi Padre'
+        alt='La Casa de mi Padre - Servicio de Dedicación'
         layout='fill'
         objectFit="cover"
         quality={100}
+        placeholder='blur'
       />
     </div>
   </div>
