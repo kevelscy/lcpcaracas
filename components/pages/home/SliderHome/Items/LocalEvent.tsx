a558e28b6d64f25d44e29d78144be9a68926d1be
kevelscy <kevelscy@gmail.com>
2022-02-16 21:38:54 -0400

    update: banner + remove event EDC

--- components/pages/home/SliderHome/Items/LocalEvent.tsx
+++ components/pages/home/SliderHome/Items/LocalEvent.tsx
@@ -1,31 +1,31 @@
 import Image from 'next/image'
-import eventSrcDesktop from 'public/img/banner/event-desktop.webp'
-import eventSrcMobile from 'public/img/banner/event-mobile.webp'
+// import eventSrcDesktop from 'public/img/banner/event-desktop.webp'
+// import eventSrcMobile from 'public/img/banner/event-mobile.webp'
 import temaDesktopSrc from 'public/img/banner/temaDesktop.webp'
 import temaMobileSrc from 'public/img/banner/temaMobile.webp'
 
 export const LocalEvent = () => (
   <div className='h-full'>
     <div className='hidden sm:block'>
-      <Image
+      {/* <Image
         src={eventSrcDesktop}
         alt='La Casa de mi Padre - Servicio de Dedicación'
         layout='fill'
         objectFit="cover"
         quality={100}
         placeholder='blur'
-      />
+      /> */}
     </div>
 
     <div className='sm:hidden'>
-      <Image
+      {/* <Image
         src={eventSrcMobile}
         alt='La Casa de mi Padre - Servicio de Dedicación'
         layout='fill'
         objectFit="cover"
         quality={100}
         placeholder='blur'
-      />
+      /> */}
     </div>
   </div>
 )
--- pages/eventos/index.tsx
+++ pages/eventos/index.tsx
@@ -1,12 +1,12 @@
 import Head from 'next/head'
-// import Link from 'next/link'
+import Link from 'next/link'
 
 // import { useEvents } from 'lib/hooks/useEvents'
 import { Page, ReactNode } from 'lib/types'
 import MainLayout from 'layouts/Main'
 import { config } from 'config'
 
-import { Event } from 'components/pages/eventos/Event'
+// import { Event } from 'components/pages/eventos/Event'
 
 export const EventosPage: Page = () => {
   // const { events } = useEvents()
@@ -20,7 +20,12 @@ export const EventosPage: Page = () => {
 
       <div className='h-screen-15 w-full'>
         <div className='w-full h-full flex flex-col justify-center items-center pt-32'>
-          <span className='text-3xl md:text-5xl font-black text-gray-900'>Eventos</span>
+          <span className='text-4xl md:text-7xl font-black text-gray-900'>Sin Eventos</span>
+          <br />
+          <Link href='/'>
+            <a className='text-lg text-secondary-500 xl:text-xl underline underline-offset-1'>Ir al inicio</a>
+          </Link>
+          {/* <span className='text-3xl md:text-5xl font-black text-gray-900'>Eventos</span>
           <br />
 
           <ul className='flex flex-wrap justify-center items-center max-w-5xl gap-x-2'>
@@ -32,7 +37,7 @@ export const EventosPage: Page = () => {
                 redirectLabel='Inscripción'
               />
             </li>
-          </ul>
+          </ul> */}
 
           {/* {
             events.length !== 0
