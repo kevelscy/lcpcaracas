34f3fd15f8a1f0dae99f376305177c2b33f2a5f1
kevelscy <67594059+kevelscy@users.noreply.github.com>
2022-03-11 05:55:40 -0800

    Update TicketReservation.tsx

--- components/pages/reservaciones/FormReservation/TicketReservation.tsx
+++ components/pages/reservaciones/FormReservation/TicketReservation.tsx
@@ -30,7 +30,7 @@ export const TicketReservation = ({ reservations, ticketData, closeTicket }: ITi
         </header>
 
         <section className='mt-4'>
-          <p>Fecha: 06 de Marzo 2022</p>
+          <p>Fecha: 13 de Marzo 2022</p>
           <p>Nombre: {ticketData.fullName.trim()}</p>
           <p>Hora: {reservations[ticketData.worship].hour}</p>
           <p>Asientos: {ticketData.chairs}</p>
