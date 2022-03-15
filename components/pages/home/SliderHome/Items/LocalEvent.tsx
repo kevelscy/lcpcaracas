71f999bba98f5c3c808da1b539f2a1e59ce354eb
kevelscy <kevelscy@gmail.com>
2022-03-15 11:39:12 -0400

    update: banner domingo

--- components/pages/home/SliderHome/Items/LocalEvent.tsx
+++ components/pages/home/SliderHome/Items/LocalEvent.tsx
@@ -1,8 +1,9 @@
 import Image from 'next/image'
+
 import eventSrcDesktop from 'public/img/banner/event-desktop.webp'
 import eventSrcMobile from 'public/img/banner/event-mobile.webp'
-import temaDesktopSrc from 'public/img/banner/temaDesktop.webp'
-import temaMobileSrc from 'public/img/banner/temaMobile.webp'
+import temaDomingoDesktopSrc from 'public/img/banner/temaDesktop.webp'
+import temaDomingoMobileSrc from 'public/img/banner/temaMobile.webp'
 
 export const LocalEvent = () => (
   <div className='h-full'>
@@ -34,7 +35,7 @@ export const LocalEvent2 = () => (
   <div className='h-full'>
     <div className='hidden sm:block'>
       <Image
-        src={temaDesktopSrc}
+        src={temaDomingoDesktopSrc}
         alt='La Casa de mi Padre - Servicio de Dedicación'
         layout='fill'
         objectFit="cover"
@@ -45,7 +46,7 @@ export const LocalEvent2 = () => (
 
     <div className='sm:hidden'>
       <Image
-        src={temaMobileSrc}
+        src={temaDomingoMobileSrc}
         alt='La Casa de mi Padre - Servicio de Dedicación'
         layout='fill'
         objectFit="cover"
