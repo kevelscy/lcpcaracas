aac61a4355a62137335d81d69ff76735c88addb2
kevelscy <kevelscy@gmail.com>
2022-04-19 18:17:42 -0400

    update: getFiles devotionals

--- pages/devocionales.tsx
+++ pages/devocionales.tsx
@@ -1,7 +1,25 @@
 import MainLayout from 'layouts/Main'
 import { Page, ReactNode } from 'lib/types'
+import { useEffect, useState } from 'react'
 
 const DevocionalesPage: Page = () => {
+  const [files, setFiles] = useState([])
+
+  const getFiles = async () => {
+    const res = await fetch('/devotionals/Aleluya Por Siempre Bateria.pdf')
+
+    const fileRes = {
+      title: decodeURI(res.url),
+      url: res.url
+    }
+
+    setFiles(prevState => ([...prevState, fileRes]))
+  }
+
+  useEffect(() => {
+    getFiles()
+  }, [])
+
   return (
     <div className="w-full h-screen flex items-center justify-center">
       <h1 className="text-3xl">Hello, World!</h1>
