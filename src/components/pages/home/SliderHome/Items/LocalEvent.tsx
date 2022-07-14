73e682892bbf863688a35835694d5c7eecba2f5f
kevelscy <kevelscy@gmail.com>
2022-07-14 08:19:18 -0400

    update local img to banner

--- src/components/pages/home/SliderHome/Items/LocalEvent.tsx
+++ src/components/pages/home/SliderHome/Items/LocalEvent.tsx
@@ -3,8 +3,9 @@ import Image from 'next/image'
 // import eventSrcDesktop from 'public/img/banner/event-desktop.webp'
 // import eventSrcMobile from 'public/img/banner/event-mobile.webp'
 
-// import bannerDesktopSrc from 'public/img/banner/temaDesktop.webp'
-// import bannerMobileSrc from 'public/img/banner/temaMobile.webp'
+// import bannerDesktopSrc from 'public/img/banner/banner-desktop.webp'
+// import bannerMobileSrc from 'public/img/banner/banner-mobile.webp'
+
 
 export const LocalEvent = () => (
   <div className='h-full'>
@@ -34,26 +35,28 @@ export const LocalEvent = () => (
 
 export const LocalEvent2 = () => (
   <div className='h-full'>
-    {/* <div className='hidden sm:block'>
+    <div className='hidden sm:block'>
       <Image
-        src={bannerDesktopSrc}
+        // src={bannerDesktopSrc}
+        src='/img/banner/banner-desktop.webp'
         alt='La Casa de mi Padre - Servicio Domingo'
         layout='fill'
         objectFit="cover"
         quality={100}
-        placeholder='blur'
+        // placeholder='blur'
       />
     </div>
 
     <div className='sm:hidden'>
       <Image
-        src={bannerMobileSrc}
+        // src={bannerMobileSrc}
+        src='/img/banner/banner-mobile.webp'
         alt='La Casa de mi Padre - Servicio de Domingo'
         layout='fill'
         objectFit="cover"
         quality={100}
-        placeholder='blur'
+        // placeholder='blur'
       />
-    </div> */}
+    </div>
   </div>
 )
