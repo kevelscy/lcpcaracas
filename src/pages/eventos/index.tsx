5f5951e84b6c7db5290c2f3c0a6c3a184d814a46
kevelscy <kevinblanco@turpialdev.com>
2023-01-29 07:58:14 -0400

    enable 'inscripciones Escuela de Crecimiento'

--- src/pages/eventos/index.tsx
+++ src/pages/eventos/index.tsx
@@ -24,8 +24,8 @@ export const EventosPage: Page = () => {
 
       <div className='h-screen-15 w-full'>
         <div className='w-full h-full flex flex-col justify-center items-center pt-32'>
-          <span className='text-4xl md:text-7xl font-black text-gray-900'>Sin Eventos</span>
-          {/* <br />
+          <span className='text-4xl md:text-7xl font-black text-gray-900'>Eventos</span>
+          <br />
 
           <ul className='flex flex-wrap justify-center items-center max-w-5xl gap-x-2'>
             <li>
@@ -33,10 +33,10 @@ export const EventosPage: Page = () => {
                 localImage={imgSrcEvent}
                 title='Inscripciones EDC'
                 redirectTo='/eventos/edc'
-                redirectLabel='Inscripción'
+                redirectLabel='Inscripciones EDC'
               />
             </li>
-          </ul> */}
+          </ul>
 
           {/* <Link href='/'>
             <a className='text-lg text-secondary-500 xl:text-xl underline underline-offset-1'>Ir al inicio</a>
